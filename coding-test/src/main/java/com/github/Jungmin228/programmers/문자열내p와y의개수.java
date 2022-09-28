42098f723c62f458307b7addc01cf2cc81983fa3
moomink <moomink228@gmail.com>
2022-09-28 18:56:54 +0900

    #7) 프로그래머스 - 문자열 내 p 와 y 의 개수

---
+++ coding-test/src/main/java/com/github/Jungmin228/programmers/문자열내p와y의개수.java
@@ -0,0 +1,17 @@
+package com.github.Jungmin228.programmers;
+
+public class 문자열내p와y의개수 {
+    class Solution {
+        boolean solution(String s) {
+            s = s.toLowerCase();
+            long p = s.chars().filter(c -> c == 'p').count();
+            long y = s.chars().filter(c -> c == 'y').count();
+
+            if (p == y) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
