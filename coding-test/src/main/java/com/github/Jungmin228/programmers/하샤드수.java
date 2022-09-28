ca1c0f1b8a36164599a8240ca84578eed3d2a02a
moomink <moomink228@gmail.com>
2022-09-28 19:08:33 +0900

    #8) 프로그래머스 - 하샤드 수

---
+++ coding-test/src/main/java/com/github/Jungmin228/programmers/하샤드수.java
@@ -0,0 +1,16 @@
+package com.github.Jungmin228.programmers;
+
+public class 하샤드수 {
+    class Solution {
+        public boolean solution(int x) {
+            String s = String.valueOf(x);
+
+            int sum = s.chars().sum() - s.length() * '0';
+
+            if (x % sum == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
