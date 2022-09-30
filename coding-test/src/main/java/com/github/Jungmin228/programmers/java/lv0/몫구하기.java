ce2160c59609bb9769cfd58fa618b55320019ab9
moomink <moomink228@gmail.com>
2022-10-01 01:27:53 +0900

    #21) 프로그래머스 - 몫 구하기

---
+++ coding-test/src/main/java/com/github/Jungmin228/programmers/java/lv0/몫구하기.java
@@ -0,0 +1,9 @@
+package com.github.Jungmin228.programmers.java.lv0;
+
+public class 몫구하기 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 / num2;
+        }
+    }
+}
