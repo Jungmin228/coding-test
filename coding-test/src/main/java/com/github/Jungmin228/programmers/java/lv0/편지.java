196c192c201ca0074e30482574a0c164cd730f31
moomink <moomink228@gmail.com>
2022-10-01 01:39:59 +0900

    #30) 프로그래머스 - 편지

---
+++ coding-test/src/main/java/com/github/Jungmin228/programmers/java/lv0/편지.java
@@ -0,0 +1,9 @@
+package com.github.Jungmin228.programmers.java.lv0;
+
+public class 편지 {
+    class Solution {
+        public int solution(String message) {
+            return message.length() * 2;
+        }
+    }
+}
