ed1e8abcef1c99885900a431c223472c8a851c2f
moomink <moomink228@gmail.com>
2022-10-01 01:38:32 +0900

    #29) 프로그래머스 - 두 수의 나눗셈

---
+++ coding-test/src/main/java/com/github/Jungmin228/programmers/java/lv0/두수의나눗셈.java
@@ -0,0 +1,9 @@
+package com.github.Jungmin228.programmers.java.lv0;
+
+public class 두수의나눗셈 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            return (int)(((double)num1 / (double)num2) * 1000);
+        }
+    }
+}
