538ad775d40f683ceb65b818d82f2b2748872e0b
moomink <moomink228@gmail.com>
2022-11-11 18:54:20 +0900

    #63) SWEA - 간단한 N 의 약수

--- coding-test/src/main/java/com/github/Jungmin228/Main.java
+++ coding-test/src/main/java/com/github/Jungmin228/Main.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T1, T2;
-        T1 = sc.nextInt();
-        T2 = sc.nextInt();
+        int T;
+        T = sc.nextInt();
 
-        switch (T1 - T2) {
-            case 1: case -2:
-                System.out.println("A");
-                break;
-            case -1: case 2:
-                System.out.println("B");
-                break;
+        for (int i = 1; i <= T; i++) {
+            if (T % i == 0) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
---
+++ coding-test/src/main/java/com/github/Jungmin228/swea/d1/Q1933간단한N의약수.java
@@ -0,0 +1,5 @@
+package com.github.Jungmin228.swea.d1;
+
+public class Q1933간단한N의약수 {
+
+}
