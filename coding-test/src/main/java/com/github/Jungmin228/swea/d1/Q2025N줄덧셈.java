5e81d4c527d93cdb9d45b9f8edbe365a6dcea93a
moomink <moomink228@gmail.com>
2022-11-13 00:55:48 +0900

    코드 정리

--- coding-test/src/main/java/com/github/Jungmin228/swea/d1/Q2025N줄덧셈.java
+++ coding-test/src/main/java/com/github/Jungmin228/swea/d1/Q2025N줄덧셈.java
@@ -1,4 +1,21 @@
 package com.github.Jungmin228.swea.d1;
 
+import java.util.Scanner;
+
 public class Q2025N줄덧셈 {
+    class Solution {
+        public /*static*/ void main(String args[]) throws Exception {
+            Scanner sc = new Scanner(System.in);
+            int T;
+            T = sc.nextInt();
+            int sum = 0;
+
+            for (int i = 1; i <= T; i++) {
+                sum += i;
+            }
+            // System.out.println((T * (T + 1)) / 2);
+
+            System.out.println(sum);
+        }
+    }
 }
